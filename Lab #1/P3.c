e9b73823395b66806b415b8e834aa9c18d85cbd3
Constantin Denis <denis.io.constantin@gmail.com>
2023-08-13 10:08:03 +0300

    Create P3.c

---
+++ Lab #1/P3.c
@@ -0,0 +1,12 @@
+/*Scrieți un program care declară și inițializează o variabilă întreagă fără semn, o variabilă reală cu reprezentare scurtă, o variabilă de tip caracter fără semn, o variabilă reală cu dublă precizie cu reprezentare lungă, cu valori corespunzătoare, arbitrare.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned natural = 1;
+    float real1 = 3.5;
+    unsigned char x = 'a';
+    long double real2 = 56.4356;
+}
