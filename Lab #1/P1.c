196964844e527fc62989dcd501051693dccb835b
Constantin Denis <denis.io.constantin@gmail.com>
2023-08-13 10:07:06 +0300

    Create P1.c

---
+++ Lab #1/P1.c
@@ -0,0 +1,14 @@
+/* Scrieți un program care declară cate o variabilă pentru fiecare tip fundamental disponibil în limbajul C (exluzând tipul void).*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    signed numar;
+    unsigned natural;
+    int intreg;
+    float real1;
+    double real2;
+    char caracter;
+}
