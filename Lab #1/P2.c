fc4ab1a9a89a5cbe8c0354eb43b56b61e9f2c02e
Constantin Denis <denis.io.constantin@gmail.com>
2023-08-13 10:07:34 +0300

    Create P2.c

---
+++ Lab #1/P2.c
@@ -0,0 +1,14 @@
+/*Scrieți un program care declară cate o variabilă pentru fiecare tip fundamental disponibil în limbajul C (exluzând tipul void), și apoi le inițializează cu valori arbitrare (orice valoare de tipul respectiv de dată).*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    signed numar = 12;
+    unsigned natural = 2;
+    int intreg = -5;
+    float real1 = 2.65;
+    double real2 = 3.6573;
+    char x = 'a';
+}
