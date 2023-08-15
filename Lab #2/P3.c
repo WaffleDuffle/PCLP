0cf8eea6911ca448ba8b8bf0ea314820ad4216cd
Constantin Denis <denis.io.constantin@gmail.com>
2023-08-15 11:03:42 +0300

    Update P3.c

--- Lab #2/P3.c
+++ Lab #2/P3.c
@@ -1,7 +1,7 @@
 /*Scrieţi un program care citește de la tastatură doi întregi, și afișează restul împărțirii primului la al doilea.*/
 
 #include <stdio.h>
-using namespace std;
+
 int a, b;
 int main()
 {
