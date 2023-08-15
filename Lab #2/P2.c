bc1c518111c2b31b1d01e7488dd27779c18b4382
Constantin Denis <denis.io.constantin@gmail.com>
2023-08-15 11:02:19 +0300

    Create P2.c

---
+++ Lab #2/P2.c
@@ -0,0 +1,12 @@
+/*Scrieţi un program care calculează şi afişează pe ecran media aritmetică a două numere întregi citite de la tastatură.*/
+
+#include <stdio.h>
+using namespace std;
+int a, b;
+float ma;
+int main()
+{
+    scanf("%d %d", &a, &b);
+    ma = (a+b)/2.0;
+    printf("%.2f", ma);
+}
