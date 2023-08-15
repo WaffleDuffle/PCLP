f0d4d2e601c265bd50a34e3c750806df69e4eb0e
Constantin Denis <denis.io.constantin@gmail.com>
2023-08-15 11:03:31 +0300

    Create P4.c

---
+++ Lab #2/P4.c
@@ -0,0 +1,11 @@
+/*Scrieţi un program care citește de la tastatură un întreg, realizează împărțirea acestuia la 2 folosind strict operații pe biți, și afișează rezultatul în consolă.*/
+
+#include <stdio.h>
+
+int a;
+int main()
+{
+    scanf("%d", &a);
+    a >>= 1;
+    printf("%d", a);
+}
